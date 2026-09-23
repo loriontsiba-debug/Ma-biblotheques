734319537051b5bd531224e31d51abd5cf11ab9f
lorion <loriontsiba@gmail.com>
2026-09-23 22:13:21 +0200

    mis a jour

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const validation = require('./middlewares/validation')
 
 const PORT= process.env.PORT || 5000;
 //midleware pour la gestion d'erreur
-app.use(express.static(path.join(__dirname, ('public'))))
+app.use(express.static(path.join(__dirname, ('public'))));
+app.get('/', (req, res)=>{
+    res.send('bienvenu sur votre api')
+})
 app.use(cors());
 app.use(logger)
 app.use(arrorHandler)
