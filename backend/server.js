a88c7023719b8233aea777535ff78874aedf7288
lorion <loriontsiba@gmail.com>
2026-09-23 23:23:43 +0200

    verification

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ const validation = require('./middlewares/validation')
 const PORT= process.env.PORT || 5000;
 //midleware pour la gestion d'erreur
 app.use(express.static(path.join(__dirname, ('public'))));
-app.get('/', (req, res)=>{
+/*app.get('/', (req, res)=>{
     res.send('bienvenu sur votre api')
-})
+})*/
 app.use(cors());
 app.use(logger)
 app.use(arrorHandler)
